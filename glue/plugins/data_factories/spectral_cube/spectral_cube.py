043efd86c4fd63b1f0bd866bd3045ab8327321e9
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-12-08 16:42:45 -0700

    Added CASA image importer

--- glue/plugins/data_factories/spectral_cube/spectral_cube.py
+++ glue/plugins/data_factories/spectral_cube/spectral_cube.py
@@ -2,8 +2,9 @@ from __future__ import absolute_import, division, print_function
 
 from spectral_cube import SpectralCube, StokesSpectralCube
 
+from qtpy.compat import getexistingdirectory
 from glue.core import Data
-from glue.config import data_factory, qglue_parser
+from glue.config import data_factory, qglue_parser, importer
 from glue.core.data_factories.fits import is_fits
 from glue.core.coordinates import coordinates_from_wcs
 
@@ -33,6 +34,7 @@ def spectral_cube_to_data(cube, label=None):
 
     result = Data(label=label)
     result.coords = coordinates_from_wcs(cube.wcs)
+    result.meta = dict(cube.header)
 
     for component in cube.components:
         data = getattr(cube, component).unmasked_data[...]
@@ -54,3 +56,18 @@ def read_spectral_cube(filename, **kwargs):
 @qglue_parser((SpectralCube, StokesSpectralCube))
 def parse_spectral_cube(cube, label):
     return [spectral_cube_to_data(cube, label=label)]
+
+
+@importer("Import CASA .image spectral cube")
+def import_casa_spectral_cube():
+
+    caption = ('Select .image directory')
+
+    directory = getexistingdirectory(caption=caption)
+    if not directory:
+        return []
+
+    data = read_spectral_cube(directory)
+    data.label = directory.replace('.image', '')
+
+    return [data]
